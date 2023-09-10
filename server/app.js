07a8c38ad68caf6cdd66288d8d8663d59cbbf499
kedar <kedarnathgubbi.1999@gmail.com>
2023-09-09 20:40:23 -0400

    remove mongo connection 2

--- server/app.js
+++ server/app.js
@@ -28,7 +28,6 @@ app.use("/api/songs/", songRoute);
 //   console.log("Mongodb Connected");
 // });
 
-// mongoose.connect("mongodb+srv://admin:admin@cluster0.vz3ejbb.mongodb.net/?retryWrites=true&w=majority", { useNewUrlParser: true });
 mongoose.connection
   .once("open", () => console.log("Connected"))
   .on("error", (error) => {
